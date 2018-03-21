4f90e57f8d5f10c1832d340efeff0fb2ed2e2b48
Jessica <jbrown15@wou.edu>
2018-03-21 00:46:13 -0700

    3

--- project3/assets/script.js
+++ project3/assets/script.js
@@ -1,24 +1,9 @@
 $(document).ready(function(){
 
 	console.log('my js is working');
-/*
 
-	$('.move').hide();
-	$('.still').hover(function(event) {
-		$(this).hide();
-		$('.move').show();
-	}, $('.still').mouseout(function(){
-		$(this).show();
-		$('.move').hide();
-	}));
-
-	
-
-	$('.still').mouseleave(function(event) {
-		$(this).show();
-		$('.move').hide();
-	});
-
-*/
+	//$('#paper').hover(function(event){
+	//	$('body').find('img').not('#paper-gif').hide();
+	//	});
 
 });
